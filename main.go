514fcdbafc42e2ba8b932657bc67c443bc993a90
crossoverJie <crossoverjie@gmail.com>
2023-09-07 09:35:30 +0800

    service

--- main.go
+++ main.go
@@ -4,12 +4,23 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
 	http.HandleFunc("/ping", func(w http.ResponseWriter, r *http.Request) {
-		log.Println("ping")
+		name, _ := os.Hostname()
+		log.Printf("%s ping", name)
 		fmt.Fprint(w, "pong")
+	})
+	http.HandleFunc("/service", func(w http.ResponseWriter, r *http.Request) {
+		resp, err := http.Get("http://k8s-combat-service:8081/ping")
+		if err != nil {
+			log.Println(err)
+			fmt.Fprint(w, err)
+			return
+		}
+		fmt.Fprint(w, resp.Status)
 	})
 
 	http.ListenAndServe(":8081", nil)
